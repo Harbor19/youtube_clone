ca4d5fa578c8aa68c31f1fc2b46de0b9e7ef7aff
Abolade Michael <jinaduabolade@gmail.com>
2024-03-28 09:13:10 +0100

    fourth commit

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -14,7 +14,7 @@ const VideoDetail = () => {
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) => setVideoDetail(data?.items[0]))
 
-    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then((data)=> setVideos(data.items)) 
+    fetchFromAPI(`search?part=isnippet&relatedToVideoId=${id}&type=video`).then((data)=> setVideos(data.items)) 
   }, [id])
 
   console.log(videos)
